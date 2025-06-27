3b27ff0684592e6c9fe332882368c8432cefd2ff
Matan <matanhershberg@gmail.com>
2025-06-27 18:13:17 -0500

    Add ESLint checks

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  "frontend/**/*.{js,jsx,ts,tsx}": "prettier --write",
+  "frontend/**/*.{js,jsx,ts,tsx}": [
+    "npm run --prefix frontend format",
+    "npm run --prefix frontend lint",
+  ],
 };
