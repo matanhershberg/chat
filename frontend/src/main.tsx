487e46f2361da6e1d4a0b845d7247db4995a7dbc
Matan <matanhershberg@gmail.com>
2025-07-09 19:19:36 -0500

    Add dark theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,20 @@
 import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
