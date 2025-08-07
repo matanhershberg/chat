309516ea581465c4ba5428b54eaeb4aea374dfe4
Matan Hershberg <matanhershberg@gmail.com>
2025-08-07 16:21:04 -0500

    Add multistage build for dockerfile, change CMD to expose ports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// Need to expose ports to outside connections to be able to connect to the docker instance
-const shouldExposePorts = process.env.EXPOSE_PORTS === "true";
-
-const configurationToExposePorts = {
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: 4173,
-  },
-};
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  ...(shouldExposePorts ? configurationToExposePorts : {}),
 });
